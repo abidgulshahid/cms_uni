fade4678f3313387139b989d8aa8b3fd7a06f06e
Abid Gul Shahid <silencemind55@gmail.com>
2020-10-15 22:35:37 +0500

    add registration table to students.

--- test_app/hod/models.py
+++ test_app/hod/models.py
@@ -3,12 +3,18 @@ from django.db import models
 # Create your models here.
 
 class department_hods(models.Model):
-    full_name = models.CharField(max_length=50)
-    cnic = models.DecimalField(max_digits=15)
-    email = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=50,help_text="Enter Your Full Name")
+    cnic = models.CharField(max_length=15,help_text="Enter Your Correct CNIC")
+    email = models.CharField(max_length=20, help_text='Enter Your Email')
+    password = models.CharField(max_length=100, help_text='Enter Your Password')
+
+    def __str__(self):
+        return self.full_name
 
 class exams(models.Model):
-    pass
+    final_result_date = models.CharField(max_length=40)
+    final_exam_date = models.CharField(max_length=40)
+    mid_result_date = models.CharField(max_length=30)
+    mid_final_date = models.CharField(max_length=40)
 
-    
+
--- test_app/students/models.py
+++ test_app/students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class registration(models.Model):
+    full_name = models.CharField(max_length=50,help_text="Enter Your Full Name")
+    cnic = models.CharField(max_length=15,help_text="Enter Your Correct CNIC")
+    email = models.CharField(max_length=20, help_text='Enter Your Email')
+    password = models.CharField(max_length=100, help_text='Enter Your Password')
+    
+    def __str__(self):
+        return self.full_name
